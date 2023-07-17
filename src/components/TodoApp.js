85e310f0aae80a36652c7671832d3d1884f896d5
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-18 00:11:32 +0300

    hello react

---
+++ src/components/TodoApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class TodoApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Todo App</h1>
+        {/* Add more components here */}
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
