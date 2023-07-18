65622c79cfd5eced7e2de2e8f9cb0cfdd8720084
HENOK MEKONNEN <Henokme@ethiopianairlines.com>
2023-07-18 14:31:38 +0300

    created todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return   <h1>Hello, React!</h1>;
-
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700 custom">
+                <h1 className="text-xl font-bold leading-none text-gray-900 dark:text-white text-center mb-3">
+                    To Do List
+                </h1>
+                <TodoList />
+            </div>
+        </div>
+    );
 }
 
 export default App;
